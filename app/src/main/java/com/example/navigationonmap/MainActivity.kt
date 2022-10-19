c5022c94bd34e8299bc5d66f2a01f4d4c652fefd
Jatin Sharma <jbhardwaj304@gmail.com>
2022-10-19 16:02:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navigationonmap/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.navigationonmap
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        replaceFragment(
+            R.id.fragment_container,
+            MapFragment.newInstance()
+        )
+    }
+
+    fun replaceFragment(id: Int, fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(id, fragment, fragment.javaClass.simpleName)
+        transaction.commitAllowingStateLoss()
+    }
+}
